09f1c52b29db5e82a8c22db834872286ebcf4c8f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-30 10:33:05 +0000

    Trying diff chart timing.

---
+++ lib/pages/test/test_widget.dart
@@ -0,0 +1,253 @@
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_charts.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/instant_timer.dart';
+import '/custom_code/actions/index.dart' as actions;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'test_model.dart';
+export 'test_model.dart';
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({
+    super.key,
+    this.devicee,
+  });
+
+  final BTDeviceStruct? devicee;
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  late TestModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => TestModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.instantTimer = InstantTimer.periodic(
+        duration: Duration(milliseconds: 1000),
+        callback: (timer) async {
+          _model.dataaa = await actions.receiveData(
+            widget!.devicee!,
+          );
+          _model.listt = await actions.receiveAndPlotData(
+            _model.dataaa!,
+          );
+          _model.textT = _model.listt!.toList().cast<double>();
+          safeSetState(() {});
+          FFAppState().addToXaxis(valueOrDefault<double>(
+            _model.listt?.first,
+            0.0,
+          ));
+          FFAppState().addToYaxis(valueOrDefault<double>(
+            _model.listt?.last,
+            0.0,
+          ));
+          safeSetState(() {});
+        },
+        startImmediately: true,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          leading: FlutterFlowIconButton(
+            borderColor: Colors.transparent,
+            borderRadius: 30.0,
+            borderWidth: 1.0,
+            buttonSize: 60.0,
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: Color(0xFF14181B),
+              size: 30.0,
+            ),
+            onPressed: () async {
+              context.pop();
+            },
+          ),
+          title: Text(
+            valueOrDefault<String>(
+              _model.dataa,
+              '-',
+            ),
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  fontFamily: 'Montserrat',
+                  color: FlutterFlowTheme.of(context).primary,
+                  fontSize: 22.0,
+                  letterSpacing: 0.0,
+                ),
+          ),
+          actions: [],
+          centerTitle: true,
+          elevation: 2.0,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Align(
+                  alignment: AlignmentDirectional(0.0, 0.0),
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    constraints: BoxConstraints(
+                      minWidth: double.infinity,
+                      minHeight: double.infinity,
+                      maxWidth: double.infinity,
+                      maxHeight: double.infinity,
+                    ),
+                    decoration: BoxDecoration(
+                      color: FlutterFlowTheme.of(context).secondaryBackground,
+                    ),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional(0.0, 0.0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 230.0,
+                            child: FlutterFlowLineChart(
+                              data: [
+                                FFLineChartData(
+                                  xData: FFAppState().xaxis,
+                                  yData: FFAppState().yaxis,
+                                  settings: LineChartBarData(
+                                    color: FlutterFlowTheme.of(context).success,
+                                    barWidth: 2.0,
+                                    isCurved: true,
+                                    dotData: FlDotData(show: false),
+                                    belowBarData: BarAreaData(
+                                      show: true,
+                                      color:
+                                          FlutterFlowTheme.of(context).accent1,
+                                    ),
+                                  ),
+                                )
+                              ],
+                              chartStylingInfo: ChartStylingInfo(
+                                backgroundColor:
+                                    FlutterFlowTheme.of(context).primaryText,
+                                showGrid: true,
+                                borderColor:
+                                    FlutterFlowTheme.of(context).secondaryText,
+                                borderWidth: 3.0,
+                              ),
+                              axisBounds: AxisBounds(
+                                minX: -10.0,
+                                minY: -10.0,
+                                maxX: 10.0,
+                                maxY: 10.0,
+                              ),
+                              xAxisLabelInfo: AxisLabelInfo(
+                                reservedSize: 32.0,
+                              ),
+                              yAxisLabelInfo: AxisLabelInfo(
+                                reservedSize: 40.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: AlignmentDirectional(0.0, -0.94),
+                          child: ListView(
+                            padding: EdgeInsets.zero,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            children: [
+                              Text(
+                                valueOrDefault<String>(
+                                  _model.textT.first.toString(),
+                                  '---',
+                                ),
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Montserrat',
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                              Text(
+                                valueOrDefault<String>(
+                                  _model.textT.last.toString(),
+                                  '---',
+                                ),
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Montserrat',
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                              Text(
+                                valueOrDefault<String>(
+                                  FFAppState().xaxis.first.toString(),
+                                  '---',
+                                ),
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Montserrat',
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                              Text(
+                                valueOrDefault<String>(
+                                  FFAppState().yaxis.first.toString(),
+                                  '---',
+                                ),
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Montserrat',
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
